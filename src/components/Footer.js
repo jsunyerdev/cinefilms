ea41640d3179f7c046d070cf482c7c9f162d0f52
jsunyerdev <jordi.lopez.sune@estudiantat.upc.edu>
2023-01-15 19:32:06 +0100

    working on it

--- src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-export default function Footer () {
-    return (
-      <div className="footer text-center bg-dark text-light p-4 mt-4">© 2022 themoviesdatabase.com | All Rights Reserved | Powered by Jordi López</div>
-    );
-  }
